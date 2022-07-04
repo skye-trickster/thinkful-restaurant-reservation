b79c0c6322eff22188c487c151813bc8e93160d6
Vincent Mills <vincentmills273@gmail.com>
2022-07-03 20:48:23 -0400

    Created service features to SQL server

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,40 +1,58 @@
 const crypto = require("crypto");
 const reservations = require("../data/reservations-data");
-const service = require("./reservations.service")
+const service = require("./reservations.service");
+
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 // Use this function to assigh ID's when necessary
-const nextId = function() {
+const nextId = function () {
   return crypto.randomBytes(16).toString("hex");
+};
+
+async function reservationExists(request, response, next) {
+  const reservation = await service.find(request.params.reservationId);
+
+  if (reservation) {
+    response.locals.reservation = reservation;
+    return next();
+  }
+
+  next({
+    status: 404,
+    message: "Reservation cannot be found",
+  });
 }
 
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-
-  console.log(await service.list())
-
   res.json({
-    data: reservations,
+    data: await service.list(),
   });
 }
 
 async function create(request, response) {
-  const { data } = request.body
+  const data = request.body;
+
+  console.log(request.body);
 
   const newReservation = {
     ...data,
-    id: nextId()
-  }
+    id: nextId(),
+  };
 
   reservations.push(newReservation);
 
   response.status(201).json({ data: newReservation });
 }
 
+function read(request, response) {
+  response.json({ data: response.locals.reservation });
+}
+
 module.exports = {
   list,
-  create: [
-    create
-  ]
+  create: [asyncErrorBoundary(create)],
+  read: [asyncErrorBoundary(reservationExists), asyncErrorBoundary(read)],
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,11 +1,17 @@
-const knex = require("../db/connection")
+const knex = require("../db/connection");
 
 async function list() {
-    return knex("reservations")
-        .select("*")
-        .returning((reservations) => console.log(reservations))
+  return knex("reservations").select("*");
+}
+
+async function read(reservation_id) {
+  return knex("reservations")
+    .select("*")
+    .where({ reservation_id })
+    .then((reservations) => reservations[0]);
 }
 
 module.exports = {
-    list
-}
+  list,
+  read,
+};
