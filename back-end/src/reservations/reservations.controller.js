18b5083a5275f61a9ee9f1d41206d00a64548347
Vincent Mills <vincentmills273@gmail.com>
2022-07-07 02:42:29 -0400

    Nearly finished US-01 for backend
    - find and create operations made for service
    - added all parameter checks
    - added 404 error for missing IDs in reads
    - Still need to create list based on date

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,58 +1,147 @@
-const crypto = require("crypto");
-const reservations = require("../data/reservations-data");
 const service = require("./reservations.service");
 
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-// Use this function to assigh ID's when necessary
-const nextId = function () {
-  return crypto.randomBytes(16).toString("hex");
-};
+function createReservation(request, response, next) {
+	const { data } = request.body;
+
+	if (!data) {
+		return next({
+			status: 400,
+			message: `Missing reservation data.`,
+		});
+	}
+
+	response.locals.reservation = data;
+	next();
+}
+
+function parameterExists(parameter) {
+	return function (request, response, next) {
+		const { reservation } = response.locals;
+		const value = reservation[parameter];
+
+		if (value === undefined || value === null) {
+			return next({
+				status: 400,
+				message: `Missing parameter: ${parameter}`,
+			});
+		}
+
+		return next();
+	};
+}
+
+function isEmptyString(parameter) {
+	return function (request, response, next) {
+		const { reservation } = response.locals;
+		const value = reservation[parameter];
+
+		if (value === "") {
+			return next({
+				status: 400,
+				message: `Empty string: ${parameter}`,
+			});
+		}
+
+		return next();
+	};
+}
+
+function checkPersonMinimum(request, response, next) {
+	const { people } = response.locals.reservation;
+
+	if (typeof people !== "number" || people <= 1) {
+		return next({
+			status: 400,
+			message: "Number of people must be at least 1.",
+		});
+	}
+
+	next();
+}
+
+function checkValidDate(request, response, next) {
+	const { reservation_date } = response.locals.reservation;
+
+	if (reservation_date && new Date(reservation_date) > 0) {
+		return next();
+	}
+
+	next({
+		status: 400,
+		message: "reservation_date is not a valid date.",
+	});
+}
+
+function checkValidTime(request, response, next) {
+	const { reservation_time, reservation_date } = response.locals.reservation;
+
+	let timeCheck = new Date(`${reservation_date} ${reservation_time}`);
+	if (reservation_time && timeCheck > 0) {
+		return next();
+	}
+
+	next({
+		status: 400,
+		message: "reservation_time is not a valid time.",
+	});
+}
+
+const checkParameters = [
+	parameterExists("first_name"),
+	isEmptyString("first_name"),
+	parameterExists("last_name"),
+	isEmptyString("last_name"),
+	parameterExists("mobile_number"),
+	isEmptyString("mobile_number"),
+	parameterExists("reservation_date"),
+	checkValidDate,
+	parameterExists("reservation_time"),
+	checkValidTime,
+	parameterExists("people"),
+	checkPersonMinimum,
+];
 
 async function reservationExists(request, response, next) {
-  const reservation = await service.find(request.params.reservationId);
+	const reservation = await service.find(request.params.reservationId);
 
-  if (reservation) {
-    response.locals.reservation = reservation;
-    return next();
-  }
+	if (reservation) {
+		response.locals.reservation = reservation;
+		return next();
+	}
 
-  next({
-    status: 404,
-    message: "Reservation cannot be found",
-  });
+	next({
+		status: 404,
+		message: `Reservation ID ${request.params.reservationId} cannot be found.`,
+	});
 }
 
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: await service.list(),
-  });
+	res.json({
+		data: await service.list(),
+	});
 }
 
 async function create(request, response) {
-  const data = request.body;
-
-  console.log(request.body);
-
-  const newReservation = {
-    ...data,
-    id: nextId(),
-  };
-
-  reservations.push(newReservation);
-
-  response.status(201).json({ data: newReservation });
+	const reservation = await service.create(response.locals.reservation);
+	response.status(201).json({ data: reservation });
 }
 
 function read(request, response) {
-  response.json({ data: response.locals.reservation });
+	response.json({ data: response.locals.reservation });
 }
 
 module.exports = {
-  list,
-  create: [asyncErrorBoundary(create)],
-  read: [asyncErrorBoundary(reservationExists), asyncErrorBoundary(read)],
+	list,
+	create: [
+		createReservation,
+		checkParameters,
+		checkPersonMinimum,
+		asyncErrorBoundary(create),
+	],
+	read: [asyncErrorBoundary(reservationExists), asyncErrorBoundary(read)],
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,17 +1,22 @@
 const knex = require("../db/connection");
 
 async function list() {
-  return knex("reservations").select("*");
+	return knex("reservations").select("*");
 }
 
-async function read(reservation_id) {
-  return knex("reservations")
-    .select("*")
-    .where({ reservation_id })
-    .then((reservations) => reservations[0]);
+async function find(reservation_id) {
+	return knex("reservations").select("*").where({ reservation_id }).first();
+}
+
+async function create(reservation) {
+	return knex("reservations")
+		.insert(reservation)
+		.returning("*")
+		.then((createdReservation) => createdReservation[0]);
 }
 
 module.exports = {
-  list,
-  read,
+	list,
+	find,
+	create,
 };
