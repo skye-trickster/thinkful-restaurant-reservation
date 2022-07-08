f927b343cfff4392457a460bf47a7e3adad961cb
Vincent Mills <vincentmills273@gmail.com>
2022-07-07 21:03:49 -0400

    Passed US-01 tests
    - Able to create and view bookings
    - still need to make buttons for browsing through the days.

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import useQuery from "../utils/useQuery";
+import { next } from "../utils/date-time";
+import formatReservationTime from "../utils/format-reservation-time";
+import formatReservationDate from "../utils/format-reservation-date";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
 
@@ -9,30 +13,70 @@ import ErrorAlert from "../layout/ErrorAlert";
  * @returns {JSX.Element}
  */
 function Dashboard({ date }) {
-  const [reservations, setReservations] = useState([]);
-  const [reservationsError, setReservationsError] = useState(null);
+	const [reservations, setReservations] = useState([]);
+	const [reservationsError, setReservationsError] = useState(null);
+	const query = useQuery();
+	const dateSearch = query.get("date");
 
-  useEffect(loadDashboard, [date]);
+	if (dateSearch && dateSearch !== date) {
+		date = dateSearch;
+	}
 
-  function loadDashboard() {
-    const abortController = new AbortController();
-    setReservationsError(null);
-    listReservations({ date }, abortController.signal)
-      .then(setReservations)
-      .catch(setReservationsError);
-    return () => abortController.abort();
-  }
+	useEffect(loadDashboard, [date]);
 
-  return (
-    <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
-      </div>
-      <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
-    </main>
-  );
+	function loadDashboard() {
+		const abortController = new AbortController();
+		setReservationsError(null);
+		listReservations({ date }, abortController.signal)
+			.then(setReservations)
+			.catch(setReservationsError);
+		return () => abortController.abort();
+	}
+
+	function nextDate() {
+		console.log("hello?");
+		console.log(next(date));
+		query.set("date", next(date));
+	}
+	return (
+		<main>
+			<h1 className="text-center">Dashboard</h1>
+			<div className="d-md-flex mb-3 justify-content-around">
+				<button>Previous</button>
+				<h4 className="mb-0 text-center">Reservations for {date}</h4>
+				<a onClick={nextDate}>Next</a>
+			</div>
+			<ErrorAlert error={reservationsError} />
+			<table className="w-100">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Phone</th>
+						<th>Date</th>
+						<th>Time</th>
+						<th>People</th>
+					</tr>
+				</thead>
+				<tbody>
+					{reservations.map((reservation) => {
+						formatReservationDate(reservation);
+						formatReservationTime(reservation);
+						return (
+							<tr key={reservation.reservation_id}>
+								<td>{reservation.reservation_id}</td>
+								<td>{`${reservation.first_name} ${reservation.last_name}`}</td>
+								<td>{reservation.mobile_number}</td>
+								<td>{reservation.reservation_date}</td>
+								<td>{reservation.reservation_time}</td>
+								<td>{reservation.people}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</main>
+	);
 }
 
 export default Dashboard;
